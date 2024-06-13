819d1255bbfeffcc0c001e0c78b47e6d5caf7bc8
Peng Wang <wpcfan@gmail.com>
2024-06-13 13:49:03 +0800

    add config server

--- config-server/src/main/kotlin/com/twigcodes/configserver/ConfigServerApplication.kt
+++ config-server/src/main/kotlin/com/twigcodes/configserver/ConfigServerApplication.kt
@@ -2,10 +2,12 @@ package com.twigcodes.configserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 open class ConfigServerApplication
 
 fun main(args: Array<String>) {
--- service-a/src/main/kotlin/com/twigcodes/servicea/ServiceAApplication.kt
+++ service-a/src/main/kotlin/com/twigcodes/servicea/ServiceAApplication.kt
@@ -2,8 +2,10 @@ package com.twigcodes.servicea
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 open class ServiceAApplication
 
 fun main(args: Array<String>) {
