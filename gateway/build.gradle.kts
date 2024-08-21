0acd7395cf77e06c1807e8c98efe95e26ef4ae27
Peng Wang <wangpeng@fangxiaoer.com>
2024-08-21 12:58:40 +0800

    add keycloak

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -1,3 +1,5 @@
+val testContainerKeycloakVersion = "3.4.0"
+
 plugins {
 	id("org.jetbrains.kotlin.plugin.spring") version "2.0.10"
 }
@@ -5,9 +7,13 @@ apply(plugin = "org.jetbrains.kotlin.plugin.spring")
 dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.security:spring-security-oauth2-resource-server")
+    implementation("org.springframework.security:spring-security-oauth2-client")
+    implementation("org.springframework.security:spring-security-oauth2-jose")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
 	implementation("org.springframework.cloud:spring-cloud-starter-contract-stub-runner")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.springframework.cloud:spring-cloud-starter-contract-stub-runner")
+    testImplementation("com.github.dasniko:testcontainers-keycloak:$testContainerKeycloakVersion")
 }
