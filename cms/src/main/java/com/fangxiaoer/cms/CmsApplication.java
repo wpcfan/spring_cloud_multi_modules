ac836f5f8e081f17612493d5287279afaabc3b9d
Peng Wang <wangpeng@fangxiaoer.com>
2024-09-02 06:03:27 +0800

    remove unused config

--- cms/src/main/java/com/fangxiaoer/cms/CmsApplication.java
+++ cms/src/main/java/com/fangxiaoer/cms/CmsApplication.java
@@ -2,9 +2,7 @@ package com.fangxiaoer.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.fangxiaoer.common.error.EnableGlobalExceptionHandling;
 
-@EnableGlobalExceptionHandling
 @SpringBootApplication
 public class CmsApplication {
     public static void main(String[] args) {
---
+++ cms/src/main/java/com/fangxiaoer/cms/rest/FileAdminController.java
@@ -0,0 +1,11 @@
+package com.fangxiaoer.cms.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/admin/files")
+public class FileAdminController {
+}
